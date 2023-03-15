866982f8fcc7456608be71c8a882a7a5e9493d1e
ly1999-hub <nhly123123456789@gmail.com>
2023-03-15 11:13:55 +0700

    add post create

--- router/admin.js
+++ router/admin.js
@@ -1,6 +1,7 @@
 const express=require('express')
 const Router=express.Router()
-
+const controllerAdmin=require('../controller/admin')
+Router.post('',controllerAdmin.CreateAdmin)
 Router.get("",function(req,res){
     res.status(200).send("ok")
 })
