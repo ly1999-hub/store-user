3a18d78bb3300d5c4995cfc3abc299248949e23f
ly1999-hub <nhly123123456789@gmail.com>
2023-03-15 15:18:03 +0700

    time handler

--- log/time/timeHandler.js
+++ log/time/timeHandler.js
@@ -5,5 +5,7 @@ module.exports.timeNow=()=>{
 module.exports.toDateTime=(secs)=>{
     var t = new Date(1970, 0, 1);
     t.setSeconds(secs);
+    //normalDate = new Date(sec).toLocaleString('en-GB',{timeZone:'UTC'})
+    //normalDate: "10/08/2021, 18:08:08"
     return t;
 }
