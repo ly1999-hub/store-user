54f9022c5a1c644b8ea2c3225d20f2d1fd2647bd
ly1999-hub <nhly123123456789@gmail.com>
2023-03-15 11:13:04 +0700

    admin model

--- model/admin.js
+++ model/admin.js
@@ -1,1 +1,18 @@
+const  mongoose=require('mongoose')
 
+var userSchema=new mongoose.Schema({
+    _id:mongoose.Schema.Types.ObjectId,
+    email:String,
+    userName:String,
+    password:String,
+    root:Boolean,
+    ban:Boolean,
+    role:Array,
+    address:String,
+    avatar:String,
+    createAt:String,
+    updateAt:String,
+})
+
+var User=mongoose.model('User',userSchema,'user')
+module.exports=User
