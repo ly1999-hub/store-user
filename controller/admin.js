b4debca438aef581aa3a4d6d5c5a8efc0a467d43
ly1999-hub <nhly123123456789@gmail.com>
2023-03-19 05:24:04 +0700

    fix setHeader add Detail

--- controller/admin.js
+++ controller/admin.js
@@ -1,10 +1,13 @@
+const jwt = require('jsonwebtoken');
+var {validationResult} = require('express-validator');
+
 const Admin=require('../model/admin')
 const initObjectID=require('../log/mongodb/initObjectID')
 const timeHandler=require('../log/time/timeHandler')
+const crypt=require('../utils/crypt')
+const promissions=require('../log/const/Permissions')
 
-var {validationResult} = require('express-validator');
-
-const checkRoot=async ()=>{
+module.exports.CheckRoot=async ()=>{
     var root=await Admin.findOne({root:"true"}).exec()
     if(root==null){
         const id=initObjectID.initID()
@@ -13,10 +16,10 @@ const checkRoot=async ()=>{
             _id:id,
             email:process.env.EMAIL_ADMIN,
             userName:process.env.USERNAME_ADMIN,
-            password:process.env.PASSWORD_ADMIN,
+            password:crypt.EncryptPassword(process.env.PASSWORD_ADMIN),
             root:process.env.ROOT,
             ban:false,
-            role:[],
+            permission:[],
             address:'',
             avatar:'',
             createAt:Date.now().toString(),
@@ -28,37 +31,37 @@ const checkRoot=async ()=>{
     }
 }
 
-module.exports.CreateAdmin=async (req,res,next)=>{
-    checkRoot()
+module.exports.Create=async (req,res,next)=>{
     var reqBody=req.body
 
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      res.status(400).send(errors)
-    }
+    var newPermission=[]
+    reqBody.permission.forEach(element => {
+        const promiss=promissions.Permissions.find(promission=>promission.Code==element)
+        newPermission.push(promiss)
+    });
 
     const id=initObjectID.initID()
-
+    const encodePassword=crypt.EncryptPassword(reqBody.password)
     const newAdmin=new Admin({
         _id:id,
         email:reqBody.email,
         userName:reqBody.userName,
-        password:reqBody.password,
+        password:encodePassword,
         root:false,
         ban:false,
-        role:[],
+        permission:newPermission,
         address:reqBody.address,
         avatar:"",
         createAt:timeHandler.timeNow(),
         updateAt:timeHandler.timeNow(),
 
     })
-     await Admin.create(newAdmin).catch((error)=>{
-        console.log(error)
-     })
-    res.status(200).send("ok")
+    await Admin.create(newAdmin)
+
+    res.status(200).json("ok")
 }
 
+//
 module.exports.CheckExistByEmail=async (req,res,next)=>{
     const email=req.body.email
 
@@ -69,24 +72,45 @@ module.exports.CheckExistByEmail=async (req,res,next)=>{
     next()
 }
 
+// LoginByEmail ...
 module.exports.LoginByEmail=async(req,res,next)=>{
     var login=req.body
+    //console.log(req.headers.authorization.split(' ')[1])
 
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       res.status(400).send(errors)
     }
-
     await Admin.findOne({email:login.email}).then(user=>{
-        if(user.password==login.password){
-            res.status(200).json("ok")
+        if(crypt.CheckPassword(login.password,user.password)){
+            const token=jwt.sign({sub:String(user._id)},process.env.SECRET_KEY,{ expiresIn: '1h' })
+            res.status(200).json({token})
         }else{
-            res.status(400).json("error pass")
+            res.status(401).json("error pass")
         }
-    }).catch(error=>{
-        res.status(500).json("error")
+    }).catch(errors=>{
+        res.status(500).json(errors)
+    })
+}
+
+// Get All ...
+module.exports.All=async (req,res,next)=>{
+    console.log("id:" +req.userId)
+    await Admin.find().then((User)=>{
+        res.status(200).json(User)
+    }).catch(errors=>{
+        res.status(500).json(errors)
     })
 }
 
 
+// Get Detail ...
+module.exports.Detail=async (req,res,next)=>{
+    const id=req.params.id
+    await Admin.findById(id).then(user=>{
+        res.status(200).json(user)
+    }).catch(error=>{
+        res.status(500).json(error)
+    })
+}
 
