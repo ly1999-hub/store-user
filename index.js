922ad6a3a9d22761d9197d9c5dd4605fb98db96a
ly1999-hub <nhly123123456789@gmail.com>
2023-03-19 05:31:39 +0700

    checkRoot

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ const bodyParser=require('body-parser')
 require('dotenv').config();
 
 const initMongoDB=require('./package/mongodb/initDB')
+const controller=require('./controller/admin')
 initMongoDB.initMongoDB()
 
 app.use(bodyParser.json()) // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
+controller.CheckRoot()
 const initRouter=require('./router/init')
 
 initRouter.Init(app)
