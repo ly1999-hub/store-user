f92855c199b400f1b95af49d5739c03880c8e61d
ly1999-hub <nhly123123456789@gmail.com>
2023-03-18 15:39:18 +0700

    add post login

--- router/admin.js
+++ router/admin.js
@@ -3,6 +3,8 @@ const Router=express.Router()
 const controllerAdmin=require('../controller/admin')
 
 const validate=require('../validation/admin')
+
 Router.post('',validate.validateRegisterCustomer(),controllerAdmin.CheckExistByEmail,controllerAdmin.CreateAdmin)
 
+Router.post('/login',validate.validationLoginByEmail(),controllerAdmin.LoginByEmail)
 module.exports=Router;
