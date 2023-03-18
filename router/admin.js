d50d22dc3621cc6b4c9dd5f3a4fc2cff083ca82e
ly1999-hub <nhly123123456789@gmail.com>
2023-03-19 05:27:15 +0700

    add get Detail

--- router/admin.js
+++ router/admin.js
@@ -1,10 +1,15 @@
 const express=require('express')
 const Router=express.Router()
 const controllerAdmin=require('../controller/admin')
+const checkLogin=require('../utils/jwt')
+const checkExist=require('../utils/checkExist')
 
 const validate=require('../validation/admin')
 
-Router.post('',validate.validateRegisterCustomer(),controllerAdmin.CheckExistByEmail,controllerAdmin.CreateAdmin)
-
+Router.post('',validate.validateRegisterCustomer(),validate.CheckValidation,checkExist.CheckExistByEmail,controllerAdmin.Create)
 Router.post('/login',validate.validationLoginByEmail(),controllerAdmin.LoginByEmail)
+Router.get('/all',checkLogin.CheckToken,controllerAdmin.All)
+Router.get('/:id',checkLogin.CheckToken,controllerAdmin.Detail)
+Router.put('change-status',checkLogin.CheckToken)
+
 module.exports=Router;
